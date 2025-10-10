e4263fc8c50ff87b2a0fea5bd9db6238acefda26
Himanshu Dalai <senorhimanshu@gmail.com>
2025-10-10 08:28:25 +0530

    added tracking uri

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -15,6 +15,9 @@ dagshub_url = "https://dagshub.com"
 repo_owner = "senorhimanshu"
 repo_name = "mlops-mini-project-ci-docker"
 
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
 model_name = "my_model"
 model_uri = f"models:/{model_name}/Production"
 
